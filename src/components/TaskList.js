d0df4204a8e172cbe37d873ae7356559589f35c9
Marcin Winiarski <winiardesign@gmail.com>
2021-01-24 18:31:08 +0100

    tasks sort and complite app

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,8 +6,21 @@ const TaskList = (props) => {
   const active = props.tasks.filter(task => task.active );
   const done = props.tasks.filter(task => !task.active );
 
+  done.sort((a,b) => {
+    return b.finishDate - a.finishDate;
+  });
+
+  active.sort((a,b) => {
+    a = a.text.toLowerCase();
+    b = b.text.toLowerCase();
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+  })
+
   const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
   const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+
   return (
     <>
       <div className="active">
